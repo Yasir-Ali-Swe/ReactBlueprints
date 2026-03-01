2a91ee730b17161f9c5abd65f05a1af0849c67ec
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-01 21:21:04 +0500

    Add public pages

---
+++ src/pages/public/DoctorListingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DoctorListingPage = () => {
+  return (
+    <div className="h-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl text-primary font-bold">Welcome to CareSync</h1>
+      <p className="text-xl font-semibold">Doctor Listing Page</p>
+    </div>
+  );
+};
+
+export default DoctorListingPage;
