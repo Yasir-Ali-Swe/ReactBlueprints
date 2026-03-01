b66654213b823a5ffb025e246556fcb6058e412f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-01 20:15:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import Register from "@/pages/auth/register";
 import SendForgetPasswordRequest from "@/pages/auth/send-forget-password-request";
 import VerifyEmail from "@/pages/auth/verify-email";
 import ResetPassword from "@/pages/auth/forget-password";
+import PatientOnboarding from "./pages/patient/patient-onboarding";
+import DoctorOnboarding from "./pages/doctor/doctor-onboarding";
+import Home from "./pages/home/home";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/register" element={<Register />} />
       <Route
@@ -17,6 +21,8 @@ const App = () => {
       />
       <Route path="/auth/verify-email/:token" element={<VerifyEmail />} />
       <Route path="/auth/reset-password/:token" element={<ResetPassword />} />
+      <Route path="/patient-onboarding/:step" element={<PatientOnboarding />} />
+      <Route path="/doctor-onboarding/:step" element={<DoctorOnboarding />} />
     </Routes>
   );
 };
