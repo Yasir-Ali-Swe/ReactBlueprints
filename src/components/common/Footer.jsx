1d7a4a42e921b587d874e2acca2e2e88839fce8d
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-01 21:20:39 +0500

    Add Footer component

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-secondary py-4 px-4">
+      <p className="text-center text-secondary font-semibold">&copy; 2026 CareSync. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
