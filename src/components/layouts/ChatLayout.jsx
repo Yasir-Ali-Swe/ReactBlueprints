99d4ce8c841d700de9670633b8a0ba287a987059
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-02 22:17:52 +0500

    Update ChatLayout layout

--- src/components/layouts/ChatLayout.jsx
+++ src/components/layouts/ChatLayout.jsx
@@ -4,18 +4,23 @@ import ConversationSidebar from "../messages/ConversationSidebar";
 import MessageInput from "../messages/MessageInput";
 import MessageWindowHeader from "../messages/MessageWindowHeader";
 
-const ChatLayout = () => {
+const DesktopChatLayout = () => {
   const { conversationId } = useParams();
+
   return (
-    <div className="flex h-screen">
-      <ConversationSidebar />
-      <div className="flex-1 flex flex-col">
-        {conversationId && <MessageWindowHeader />}
+    <div className="flex h-screen w-full">
+      <ConversationSidebar
+        className={`${conversationId ? "hidden lg:block lg:w-1/4" : "block w-full lg:w-1/4"} border-r h-full`}
+      />
+      <div
+        className={`${conversationId ? "flex flex-col w-full" : "hidden lg:flex flex-col"} flex-1 h-full`}
+      >
+        {conversationId ? <MessageWindowHeader /> : null}
         <Outlet />
-        {conversationId && <MessageInput />}
+        {conversationId ? <MessageInput /> : null}
       </div>
     </div>
   );
 };
 
-export default ChatLayout;
+export default DesktopChatLayout;
