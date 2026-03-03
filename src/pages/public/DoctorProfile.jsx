c017d8389074e9b1458c21636ed1a018215ea015
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-04 02:05:56 +0500

    Add DoctorProfile page

---
+++ src/pages/public/DoctorProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookApointment = () => {
+  return (
+    <div className="h-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl text-primary font-bold">Welcome to CareSync</h1>
+      <p className="text-xl font-semibold">Doctor Profile Page</p>
+    </div>
+  );
+};
+
+export default BookApointment;
