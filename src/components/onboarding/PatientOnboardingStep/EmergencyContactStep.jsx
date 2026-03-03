5c9c4bc693bddc48a487f8cc06ab32165cce0750
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-04 02:34:44 +0500

    Update patient emergency contact step

--- src/components/onboarding/PatientOnboardingStep/EmergencyContactStep.jsx
+++ src/components/onboarding/PatientOnboardingStep/EmergencyContactStep.jsx
@@ -34,17 +34,21 @@ const EmergencyContactStep = ({ currentStep }) => {
         {/* Name */}
         <div className="space-y-2">
           <Label htmlFor="emergencyName">Full Name</Label>
-          <Input id="emergencyName" placeholder="Enter full name" />
+          <Input
+            id="emergencyName"
+            placeholder="Enter full name"
+            className={"rounded-sm"}
+          />
         </div>
 
         {/* Relationship */}
         <div className="space-y-2">
           <Label htmlFor="relationship">Relationship</Label>
           <Select>
-            <SelectTrigger>
+            <SelectTrigger className={"rounded-sm"}>
               <SelectValue placeholder="Select relationship" />
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent className={"rounded-sm"}>
               <SelectGroup>
                 <SelectLabel>Relationship</SelectLabel>
                 <SelectItem value="Father">Father</SelectItem>
@@ -60,13 +64,19 @@ const EmergencyContactStep = ({ currentStep }) => {
         {/* Phone Number */}
         <div className="space-y-2">
           <Label htmlFor="phoneNumber">Phone Number</Label>
-          <Input id="phoneNumber" placeholder="0330-0000000" type="tel" />
+          <Input
+            id="phoneNumber"
+            placeholder="0330-0000000"
+            type="tel"
+            className={"rounded-sm"}
+          />
         </div>
 
         {/* Alternate Phone Number */}
         <div className="space-y-2">
           <Label htmlFor="alternatePhoneNumber">Alternate Phone Number</Label>
           <Input
+            className={"rounded-sm"}
             id="alternatePhoneNumber"
             placeholder="0330-0000000"
             type="tel"
@@ -77,6 +87,7 @@ const EmergencyContactStep = ({ currentStep }) => {
         <div className="flex justify-between pt-6">
           <Button
             type="button"
+            className={"rounded-sm"}
             variant="outline"
             onClick={handlePrevious}
             disabled={currentStep === 1}
@@ -88,6 +99,7 @@ const EmergencyContactStep = ({ currentStep }) => {
             type="button"
             onClick={handleNext}
             disabled={currentStep === 4} // last step, could submit instead
+            className={"rounded-sm"}
           >
             Submit
           </Button>
