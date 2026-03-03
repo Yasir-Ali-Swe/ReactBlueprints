32d465d564bf030118f30bd9634aa91ae814581b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-03 22:46:41 +0500

    Update MessageWindowHeader component

--- src/components/messages/MessageWindowHeader.jsx
+++ src/components/messages/MessageWindowHeader.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Phone, Video, ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import DoctorsData from "@/dummyData/DoctorData.js";
 
 const MessageWindowHeader = () => {
+  const { id } = useParams();
   return (
     <div className="border-b h-15 w-full flex items-center justify-between">
       <div className="flex items-center gap-2 p-2">
@@ -14,14 +17,16 @@ const MessageWindowHeader = () => {
           <ArrowLeft className="size-5 text-muted-foreground" />
         </Link>
         <Avatar className="size-9">
-          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarImage src={`https://i.pravatar.cc/150?img=${id}`} />
           <AvatarFallback>YA</AvatarFallback>
         </Avatar>
         <div className="flex-1 min-w-0">
           <h1 className="text-base font-semibold text-primary truncate">
             Yasir Ali
           </h1>
-          <p className="text-sm font-semibold text-green-900 animate-pulse">typing...</p>
+          <p className="text-sm font-semibold text-green-900 animate-pulse">
+            typing...
+          </p>
         </div>
       </div>
       <div className="flex items-center justify-center gap-2 mx-4">
