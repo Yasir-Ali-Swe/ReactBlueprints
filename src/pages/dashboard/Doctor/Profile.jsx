2ba162308c1890269639c9b5043b3a6d4a1ad2ea
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:19 +0500

    Add doctor dashboard profile page

---
+++ src/pages/dashboard/Doctor/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">Doctor Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
