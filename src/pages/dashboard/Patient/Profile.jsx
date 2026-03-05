b03a62b4e529f74c1edf94de68383f1b02bc8410
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:07 +0500

    Add patient dashboard profile page

---
+++ src/pages/dashboard/Patient/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">
+        Patient Profile
+      </h1>
+    </div>
+  );
+};
+
+export default Profile;
