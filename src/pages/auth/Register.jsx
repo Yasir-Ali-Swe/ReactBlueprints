49eea90243e16facfa6ea5940c30b91d479653ca
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:42:20 +0500

    Update register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -6,10 +6,10 @@ import { Hospital } from "lucide-react";
 const RegisterPage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 h-screen overflow-hidden">
-      <div className="w-full hidden lg:flex items-center justify-center bg-primary p-2">
+      <div className="w-full hidden lg:flex items-center justify-center p-2 border-r-2 border-border">
         <ImagesGrid />
       </div>
-      <div className="w-full flex flex-col items-center justify-center h-screen bg-secondary">
+      <div className="w-full flex flex-col items-center justify-center h-screen">
         <div className="mb-5 flex items-center gap-2">
           <h1 className="text-2xl font-bold text-primary">CareSync</h1>
           <Hospital className="text-primary font-black size-9" />
