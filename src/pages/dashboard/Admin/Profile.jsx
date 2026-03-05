c479ba030065bb643d3177393583da68e06da680
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:24 +0500

    Add admin dashboard profile page

---
+++ src/pages/dashboard/Admin/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">Admin Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
