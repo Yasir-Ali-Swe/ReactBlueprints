fc117dfed840898aa7626f712523284891d4c03c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:16 +0500

    Add doctor dashboard availability page

---
+++ src/pages/dashboard/Doctor/Availability.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Availability = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">
+        Doctor Availability
+      </h1>
+    </div>
+  );
+};
+
+export default Availability;
