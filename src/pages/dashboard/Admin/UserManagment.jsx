7f49d38569fa657cf79444c25703b4353a0d0030
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:29 +0500

    Add admin dashboard user management page

---
+++ src/pages/dashboard/Admin/UserManagment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserManagment = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">Admin User Managment</h1>
+    </div>
+  );
+};
+
+export default UserManagment;
