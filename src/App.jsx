7de9a88a7f3ebeb1d7fa3dc1e8048236e7888594
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:43:07 +0500

    Update main app routing and structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Register from "@/pages/auth/Register";
 import SendForgetPasswordRequest from "@/pages/auth/SendForgetPasswordRequest";
 import VerifyEmail from "@/pages/auth/VerifyEmail";
 import ResetPassword from "@/pages/auth/ForgetPassword";
-import PatientOnboarding from "./pages/patient/PatientOnboarding";
-import DoctorOnboarding from "./pages/doctor/DoctorOnboarding";
+import PatientOnboarding from "./pages/onboarding/PatientOnboarding";
+import DoctorOnboarding from "./pages/onboarding/DoctorOnboarding";
 import Home from "./pages/public/Home";
 import Contact from "./pages/public/Contact";
 import PublicLayout from "@/components/layouts/PublicLayout";
@@ -16,6 +16,18 @@ import ChatWindowPlaceholder from "@/pages/chat/ChatWIndowPlacholder";
 import ChatWindow from "@/pages/chat/ChatWindow";
 import BookApointment from "@/pages/public/BookApointment";
 import DoctorProfile from "@/pages/public/DoctorProfile";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+import PatientStats from "@/pages/dashboard/Patient/Stats";
+import DashboardIndexRedirect from "@/components/dashboard/DashboardIndexRedirect.jsx";
+import DoctorStats from "@/pages/dashboard/Doctor/Stats";
+import AdminStats from "@/pages/dashboard/Admin/Stats";
+import PatientAppointment from "@/pages/dashboard/Patient/Appointment";
+import PatientProfile from "@/pages/dashboard/Patient/Profile";
+import DoctorAppointments from "@/pages/dashboard/Doctor/Appointments";
+import DoctorDashboardProfile from "@/pages/dashboard/Doctor/Profile";
+import DoctorAvailability from "@/pages/dashboard/Doctor/Availability";
+import AdminUserManagment from "@/pages/dashboard/Admin/UserManagment";
+import AdminProfile from "@/pages/dashboard/Admin/Profile";
 
 const App = () => {
   return (
@@ -33,6 +45,22 @@ const App = () => {
       <Route element={<ChatLayout />}>
         <Route path="/messages" element={<ChatWindowPlaceholder />} />
         <Route path="/messages/:conversationId" element={<ChatWindow />} />
+      </Route>
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<DashboardIndexRedirect role={"admin"} />} />
+        <Route path="patient/stats" element={<PatientStats />} />
+        <Route path="doctor/stats" element={<DoctorStats />} />
+        <Route path="admin/stats" element={<AdminStats />} />
+        // Patient Routes
+        <Route path="patient/appointments" element={<PatientAppointment />} />
+        <Route path="patient/profile" element={<PatientProfile />} />
+        // Doctor Routes
+        <Route path="doctor/appointments" element={<DoctorAppointments />} />
+        <Route path="doctor/profile" element={<DoctorDashboardProfile />} />
+        <Route path="doctor/availability" element={<DoctorAvailability />} />
+        //Admin Routes
+        <Route path="admin/users-management" element={<AdminUserManagment />} />
+        <Route path="admin/profile" element={<AdminProfile />} />
       </Route>
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/register" element={<Register />} />
