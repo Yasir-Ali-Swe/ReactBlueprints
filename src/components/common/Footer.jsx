ba94d09223c56619f978685e87f910bc3b4a8d13
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:42:54 +0500

    Update footer component

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-primary text-secondary py-4 px-4">
-      <p className="text-center text-secondary font-semibold">&copy; 2026 CareSync. All rights reserved.</p>
+    <div className="bg-card py-4 px-4 border-t-2 border-border shadow-sm">
+      <p className="text-center text-primary font-semibold">
+        &copy; 2026 CareSync. All rights reserved.
+      </p>
     </div>
   );
 };
