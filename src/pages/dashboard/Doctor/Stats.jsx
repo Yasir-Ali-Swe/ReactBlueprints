316d42b9f61d0819ae0b82516595f352eff4f643
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-05 19:50:22 +0500

    Add doctor dashboard stats page

---
+++ src/pages/dashboard/Doctor/Stats.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-primary text-center my-5">Doctor Stats</h1>
+    </div>
+  );
+};
+
+export default Stats;
