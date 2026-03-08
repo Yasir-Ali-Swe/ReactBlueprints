7e03ed9b456432175f7673f36c0a8fe240c6faad
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-08 20:11:37 +0500

    Update theme toggle component

--- src/components/common/ToggleTheme.jsx
+++ src/components/common/ToggleTheme.jsx
@@ -15,13 +15,13 @@ const ToggleTheme = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className={"rounded-full"}>
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="rounded-2xl">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
